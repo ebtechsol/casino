f8d0b499540f70f1a3e3989f650aefee63d2585c
Umair Majeed <umair.majeed@globalfundsolutions.com>
2025-01-10 19:44:23 +0500

    resolve routes dependancy issue against next-routes

--- app/Quiz/page.tsx
+++ app/Quiz/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { useSearchParams, redirect } from "next/navigation";
+import { useEffect } from "react";
 import { Suspense } from "react";
+import { useSearchParams, redirect } from "next/navigation";
 
 import CasinoDeals from "@/app/components/CasinoDeals";
 import FeaturedCasino from "@/app/components/FeaturedCasino";
 import Start from "@/app/components/Quizzes/Start";
 import Qz1 from "@/app/components/Quizzes/Qz1";
 import Qz2 from "@/app/components/Quizzes/Qz2";
-
 import NewsAndBlogs from "@/app/components/NewsAndBlogs";
-import { useEffect } from "react";
 
 function SelectQuiz() {
   const searchParams = useSearchParams();
